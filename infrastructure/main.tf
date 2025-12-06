fd64ce90e0aa881d1dbd94f7937246d17fae1da9
kentatixx <kentatixx@gmail.com>
2025-12-06 12:00:31 +0800

    fixing main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -22,9 +22,9 @@ resource "random_string" "random" {
 }
 
 resource "azurerm_static_web_app" "web" {
-  name                = "kenta-frontend-app-${random_string.random.result}"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  name = "kenta-frontend-app-${random_string.random.result}"
+  location            = azurerm_resource_group.rg.location  
+  resource_group_name = azurerm_resource_group.rg.name  
 }
 
 resource "azurerm_storage_account" "storage" {
@@ -55,12 +55,12 @@ resource "azurerm_cosmosdb_account" "db" {
 }
 resource "azurerm_cosmosdb_mongo_collection" "users" {
   name                = "users"
-  resource_group_name = azurerm_cosmosdb_account.db.resource_group_name
+  resource_group_name = azurerm_resource_group.rg.name
   account_name        = azurerm_cosmosdb_account.db.name
   database_name       = azurerm_cosmosdb_mongo_database.db.name
 
   default_ttl_seconds = -1
-  shard_key           = "email"
+  shard_key           = "email" 
 
 
   index {
@@ -77,16 +77,15 @@ resource "azurerm_cosmosdb_mongo_collection" "users" {
 
 
 
-
 resource "azurerm_cosmosdb_mongo_database" "db" {
-  name                = "visitors-data"
-  resource_group_name = azurerm_cosmosdb_account.db.resource_group_name
+  name                = "visitors-data" 
+  resource_group_name = azurerm_resource_group.rg.name
   account_name        = azurerm_cosmosdb_account.db.name
 }
 
 resource "azurerm_cosmosdb_mongo_collection" "visitors" {
   name                = "visitors"
-  resource_group_name = azurerm_cosmosdb_account.db.resource_group_name
+  resource_group_name = azurerm_resource_group.rg.name
   account_name        = azurerm_cosmosdb_account.db.name
   database_name       = azurerm_cosmosdb_mongo_database.db.name
 
@@ -132,7 +131,7 @@ resource "azurerm_linux_function_app" "function_app" {
         "https://${azurerm_static_web_app.web.default_host_name}",
         "https://portal.azure.com",
         "http://localhost:5173"
-      ]
+        ]
     }
   }
 }
